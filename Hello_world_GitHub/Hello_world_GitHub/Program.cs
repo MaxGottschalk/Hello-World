af8ae01dec8651f34c5eaa5ec3cfc60bd0046254
MaxGottschalk <maxjordan@hotmail.se>
2017-01-19 10:01:21 +0100

    Add files via upload

---
+++ Hello_world_GitHub/Hello_world_GitHub/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_world_GitHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyProgram myProgram = new MyProgram();
+            myProgram.Run();
+        }
+    }
+}
